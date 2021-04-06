449dee22ee40c9a4fec28f7919386dd6a7707e25
Konrad Dulemba <konrad@dulemba.me>
2021-04-06 22:31:20 +0200

    Set "/" endpoint to fix waking up by cron-jobs

--- delivery/http/server.go
+++ delivery/http/server.go
@@ -27,12 +27,17 @@ func NewHandler(e *echo.Echo, app *app.App) {
 	handler := &server{
 		app,
 	}
+	e.GET("/", handler.Index)
 	e.POST("/jar", handler.AddJar)
 	e.GET("/jar/card/:code", handler.GetCard)
 
 	//TODO: Temp for debugging purposes
 	e.GET("/jar/all/:code", handler.GetJar)
 	e.GET("/jar/resetall", handler.ResetCardsSeenThisDay)
+}
+
+func (s *server) Index(c echo.Context) error {
+	return c.JSON(http.StatusOK, ResponseMessage{Message: "OK"})
 }
 
 func (s *server) AddJar(c echo.Context) error {
